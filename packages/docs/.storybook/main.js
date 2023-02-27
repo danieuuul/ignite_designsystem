cf0c36ea3c62eee95d4fc25bf9004073d29f5262
Daniel <danieuuul@gmail.com>
2023-02-27 17:10:17 -0300

    fix: add subpath to storybook on prodution

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -13,4 +13,10 @@ module.exports = {
   features: {
     storyStoreV7: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/ignite_designsystem";
+    }
+    return config;
+  },
 };
